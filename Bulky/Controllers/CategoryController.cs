97ec8bd64d83c69b3e60bde8d9bb71f1f6c262ce
Adam Berouijil <125824@sintcordula.be>
2026-04-30 09:48:50 +0200

    Add project files.

---
+++ Bulky/Controllers/CategoryController.cs
@@ -0,0 +1,53 @@
+﻿using Bulky.Data;
+using Bulky.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bulky.Controllers
+{
+    public class CategoryController : Controller
+    {
+       
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj) {
+            //custom validation
+            if (obj.Naam == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Naam", "De naam mag niet hetzelfde zijn als de display order");
+            }
+
+            //custom validation
+            if(obj.Naam != null && obj.Naam.ToLower() =="test") {
+                ModelState.AddModelError("", "De naam mag niet 'Test' zijn");
+            }
+
+            //model validation      
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+
+    
+
+
+
+}
+
+    }
+}
---
+++ Bulky/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Bulky.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // Constructor that takes DbContextOptions and passes it to the base class constructor
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories {  get; set; }
+        
+       protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Naam = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Naam = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Naam = "History", DisplayOrder = 3 }
+            );
+        }
+    }
+}
+
